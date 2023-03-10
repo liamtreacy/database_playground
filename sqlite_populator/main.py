1bd784664e737a5550b317941d6126818d14a6f3
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-03-10 21:15:30 +0000

    New table with foriegn key relation to laptop

--- sqlite_populator/main.py
+++ sqlite_populator/main.py
@@ -20,7 +20,13 @@ def create_tables(conn):
 
     conn.execute('''
               CREATE TABLE IF NOT EXISTS laptops
-              ([student_id] INTEGER PRIMARY KEY AUTOINCREMENT, [laptop_name] TEXT)
+              ([laptop_id] INTEGER PRIMARY KEY, [laptop_name] TEXT)
+              ''')
+
+    conn.execute('''
+              CREATE TABLE IF NOT EXISTS chargers
+              ([charger_id] INTEGER PRIMARY KEY AUTOINCREMENT, [laptop_id] TEXT,
+              FOREIGN KEY(laptop_id) REFERENCES laptops(laptop_id))
               ''')
 
     conn.commit()
@@ -37,10 +43,18 @@ def populate_tables(conn, noOfEntries):
                   ''')
 
         conn.execute(f'''
-                  INSERT INTO laptops (laptop_name)
+                  INSERT INTO laptops (laptop_id, laptop_name)
                         VALUES
-                        ('{laptop_names[i%5]}')
+                        ('{i}','{laptop_names[i%5]}')
                   ''')
+
+        conn.execute(f'''
+                  INSERT INTO chargers (laptop_id)
+                        VALUES
+                        ('{i}')
+                  ''')
+
+
 
     conn.commit()
 
@@ -57,7 +71,8 @@ if __name__ == '__main__':
     db_loc = os.getcwd() + r"/database/demo.db"
     conn = create_connection(db_loc)
     create_tables(conn)
-    populate_tables(conn, 10000000)
+    #populate_tables(conn, 10000000)
+    populate_tables(conn, 10)
     close_db_connection(conn)
     print_file_size(db_loc)
     # create foriegn key relationship to have a better example of 'real world'
